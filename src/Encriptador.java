b5e1936ddc099ee971ae02641fb22d1efa72f15c
Brayan Henao <bryanhenao96@gmail.com>
2018-05-24 18:57:13 -0500

    Password match fixed

--- src/Encriptador.java
+++ src/Encriptador.java
@@ -1,60 +1,48 @@
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.security.InvalidKeyException;
-import java.security.Key;
-import java.security.NoSuchAlgorithmException;
-
-import javax.crypto.BadPaddingException;
 import javax.crypto.Cipher;
-import javax.crypto.IllegalBlockSizeException;
-import javax.crypto.NoSuchPaddingException;
 import javax.crypto.spec.SecretKeySpec;
+import java.io.File;
+import java.security.Key;
 
 public class Encriptador {
 
-	private String key;
-	byte[] inputFile;
+    private String key;
+    byte[] inputFile;
 
-	public Encriptador(String passwd, byte[] inputFile) {
-		this.inputFile = inputFile;
-		key = passwd;
+    public Encriptador(String passwd, byte[] inputFile) {
+        this.inputFile = inputFile;
+        key = passwd;
 
-	}
+    }
 
-	public byte[] encriptar() {
-		return fileProcessor(Cipher.ENCRYPT_MODE, key, inputFile);
-	}
+    public byte[] encriptar() throws Exception {
+        return fileProcessor(Cipher.ENCRYPT_MODE, key, inputFile);
+    }
 
-	public byte[] desencriptar() throws BadPaddingException {
-		return fileProcessor(Cipher.DECRYPT_MODE, key, inputFile);
-	}
+    public byte[] desencriptar() throws Exception {
+        return fileProcessor(Cipher.DECRYPT_MODE, key, inputFile);
+    }
 
-	private byte[] fileProcessor(int cipherMode, String key, byte[] inputFile) {
-		byte[] outputBytes = null;
-		try {
-			Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
-			Cipher cipher = Cipher.getInstance("AES");
-			cipher.init(cipherMode, secretKey);
+    private byte[] fileProcessor(int cipherMode, String key, byte[] inputFile) throws Exception {
+        byte[] outputBytes = null;
 
-			byte[] inputBytes = inputFile;
+        Key secretKey = new SecretKeySpec(key.getBytes(), "AES");
+        Cipher cipher = Cipher.getInstance("AES");
+        cipher.init(cipherMode, secretKey);
 
-			outputBytes = cipher.doFinal(inputBytes);
+        byte[] inputBytes = inputFile;
 
-		} catch (NoSuchPaddingException | NoSuchAlgorithmException | InvalidKeyException | BadPaddingException
-				| IllegalBlockSizeException e) {
-			e.printStackTrace();
-		}
-		return outputBytes;
-	}
+        outputBytes = cipher.doFinal(inputBytes);
 
-	public static void main(String[] args) {
-		String key = "This is a secret";
-		File inputFile = new File("./claves/Priv.key");
-		File encryptedFile = new File("./claves/Priv.encrypted");
-		File decryptedFile = new File("./claves/Priv2.key");
 
-	}
+        return outputBytes;
+    }
+
+    public static void main(String[] args) {
+        String key = "This is a secret";
+        File inputFile = new File("./claves/Priv.key");
+        File encryptedFile = new File("./claves/Priv.encrypted");
+        File decryptedFile = new File("./claves/Priv2.key");
+
+    }
 }
--- src/ProyectoSeguridad.java
+++ src/ProyectoSeguridad.java
@@ -12,229 +12,229 @@ import java.security.spec.X509EncodedKeySpec;
 
 public class ProyectoSeguridad {
 
-	public static void main(String... argv) throws Exception {
+    public static void main(String... argv) throws Exception {
 
-		System.out.println("****************************************************************************");
-		System.out.println("****************************************************************************");
-		System.out.println("*******************GENERADOR, FIRMADOR Y VERIFICADOR***********************");
-		System.out.println("********HECHO POR: -BRAYAN HENAO - SERGIO OSPINA - ESTEBAN AGUIRRE**********");
-		System.out.println("****************************************************************************");
-		System.out.println("****************************************************************************");
-		int opcion = 0;
-		do {
-			String op = JOptionPane.showInputDialog(null,
-					"Â¿QuÃ© desea hacer? Seleccione una opciÃ³n\n 1- Generar llaves"
-							+ " \n 2- Firmar un archivo\n 3- Verificar la firma de un archivo\n 4- Salir\n");
-			if (op == null) {
-				break;
-			}
-			try {
-				System.out.println(op);
-				opcion = Integer.parseInt(op);
-			} catch (NumberFormatException e) {
-				JOptionPane.showMessageDialog(null, "INGRESE UNA OPCIÃ“N VÃ�LIDA", "ERROR", JOptionPane.ERROR_MESSAGE);
-			}
+        System.out.println("****************************************************************************");
+        System.out.println("****************************************************************************");
+        System.out.println("*******************GENERADOR, FIRMADOR Y VERIFICADOR***********************");
+        System.out.println("********HECHO POR: -BRAYAN HENAO - SERGIO OSPINA - ESTEBAN AGUIRRE**********");
+        System.out.println("****************************************************************************");
+        System.out.println("****************************************************************************");
+        int opcion = 0;
+        do {
+            String op = JOptionPane.showInputDialog(null,
+                    "Â¿QuÃ© desea hacer? Seleccione una opciÃ³n\n 1- Generar llaves"
+                            + " \n 2- Firmar un archivo\n 3- Verificar la firma de un archivo\n 4- Salir\n");
+            if (op == null) {
+                break;
+            }
+            try {
+                System.out.println(op);
+                opcion = Integer.parseInt(op);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "INGRESE UNA OPCIÃ“N VÃ�LIDA", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
 
-			switch (opcion) {
-			case 1:
-				generador();
-				continue;
-			case 2:
-				try {
-					firmador();
-				} catch (BadPaddingException e) {
-					JOptionPane.showMessageDialog(null, "PRUEBAAAA");
-				}
-				
-				continue;
-			case 3:
-				verificador();
-				continue;
-			case JOptionPane.CLOSED_OPTION:
-				opcion = 4;
-				continue;
+            switch (opcion) {
+                case 1:
+                    generador();
+                    continue;
+                case 2:
+                    try {
+                        firmador();
+                    } catch (BadPaddingException e) {
+                        JOptionPane.showMessageDialog(null, "PRUEBAAAA");
+                    }
 
-			}
+                    continue;
+                case 3:
+                    verificador();
+                    continue;
+                case JOptionPane.CLOSED_OPTION:
+                    opcion = 4;
+                    continue;
 
-		} while (opcion != 4);
-	}
+            }
 
-	public static void generador() throws Exception {
-		GeneradorLlaves llaves = new GeneradorLlaves();
+        } while (opcion != 4);
+    }
 
-		String pass = JOptionPane.showInputDialog(null, "Digite una password para proteger su llave privada");
+    public static void generador() throws Exception {
+        GeneradorLlaves llaves = new GeneradorLlaves();
 
-		if (pass.length() < 16) {
-			for (int i = pass.length(); i < 16; i++) {
-				pass += '0';
-			}
-		} else if (pass.length() > 16) {
-			pass = pass.substring(0, 15);
-		}
+        String pass = JOptionPane.showInputDialog(null, "Digite una password para proteger su llave privada");
 
-		System.out.println("**************GENERANDO LLAVES*************");
+        if (pass.length() < 16) {
+            for (int i = pass.length(); i < 16; i++) {
+                pass += '0';
+            }
+        } else if (pass.length() > 16) {
+            pass = pass.substring(0, 15);
+        }
 
-		int opcion = 0;
-		do {
-			String op = JOptionPane.showInputDialog(null, "Â¿QuÃ© desea hacer? Seleccione una "
-					+ "opciÃ³n\n 1- Guardar llave pÃºblica\n 2- Guardar llave privada\n 3- Salir\n");
+        System.out.println("**************GENERANDO LLAVES*************");
 
-			if (op == null) {
-				break;
-			}
-			try {
-				opcion = Integer.parseInt(op);
-			} catch (NumberFormatException e) {
-				JOptionPane.showMessageDialog(null, "INGRESE UNA OPCIÃ“N VÃ�LIDA", "ERROR", JOptionPane.ERROR_MESSAGE);
-			}
-			int seleccion;
-			JFileChooser jf = new JFileChooser("claves/");
+        int opcion = 0;
+        do {
+            String op = JOptionPane.showInputDialog(null, "Â¿QuÃ© desea hacer? Seleccione una "
+                    + "opciÃ³n\n 1- Guardar llave pÃºblica\n 2- Guardar llave privada\n 3- Salir\n");
 
-			String path;
-			switch (opcion) {
-			case 1:
-				jf.setDialogTitle("Guardar llave pÃºblica.");
-				seleccion = jf.showSaveDialog(null);
-				if (seleccion == JFileChooser.APPROVE_OPTION) {
-					path = jf.getSelectedFile().getAbsolutePath();
-					FileOutputStream out = new FileOutputStream(new File(path + ".pub"));
-					out.write(llaves.getLlavePublica().getEncoded());
-					out.close();
-				}
-				System.err.println("Private key format: " + llaves.getLlavePublica().getFormat());
-				continue;
-			case 2:
-				jf.setDialogTitle("Guardar llave privada.");
-				seleccion = jf.showSaveDialog(null);
-				if (seleccion == JFileChooser.APPROVE_OPTION) {
-					path = jf.getSelectedFile().getAbsolutePath();
+            if (op == null) {
+                break;
+            }
+            try {
+                opcion = Integer.parseInt(op);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "INGRESE UNA OPCIÃ“N VÃ�LIDA", "ERROR", JOptionPane.ERROR_MESSAGE);
+            }
+            int seleccion;
+            JFileChooser jf = new JFileChooser("claves/");
 
-					FileOutputStream out = new FileOutputStream(new File(path + ".key"));
-					Encriptador h = new Encriptador(pass, llaves.getLlavePrivada().getEncoded());
-					out.write(h.encriptar());
-					out.close();
-				}
-				System.err.println("Private key format: " + llaves.getLlavePrivada().getFormat());
-				continue;
-			case JOptionPane.CLOSED_OPTION:
-				opcion = 3;
-				continue;
-			}
+            String path;
+            switch (opcion) {
+                case 1:
+                    jf.setDialogTitle("Guardar llave pÃºblica.");
+                    seleccion = jf.showSaveDialog(null);
+                    if (seleccion == JFileChooser.APPROVE_OPTION) {
+                        path = jf.getSelectedFile().getAbsolutePath();
+                        FileOutputStream out = new FileOutputStream(new File(path + ".pub"));
+                        out.write(llaves.getLlavePublica().getEncoded());
+                        out.close();
+                    }
+                    System.err.println("Private key format: " + llaves.getLlavePublica().getFormat());
+                    continue;
+                case 2:
+                    jf.setDialogTitle("Guardar llave privada.");
+                    seleccion = jf.showSaveDialog(null);
+                    if (seleccion == JFileChooser.APPROVE_OPTION) {
+                        path = jf.getSelectedFile().getAbsolutePath();
 
-		} while (opcion != 3);
+                        FileOutputStream out = new FileOutputStream(new File(path + ".key"));
+                        Encriptador h = new Encriptador(pass, llaves.getLlavePrivada().getEncoded());
+                        out.write(h.encriptar());
+                        out.close();
+                    }
+                    System.err.println("Private key format: " + llaves.getLlavePrivada().getFormat());
+                    continue;
+                case JOptionPane.CLOSED_OPTION:
+                    opcion = 3;
+                    continue;
+            }
 
-	}
+        } while (opcion != 3);
 
-	public static void firmador() throws Exception,BadPaddingException {
-		JOptionPane.showMessageDialog(null, "SELECCIONE EL ARCHIVO A FIRMAR");
-		JFileChooser jf = new JFileChooser();
-		jf.setCurrentDirectory(new File("archivos/"));
-		jf.setDialogTitle("Seleccionar archivo a firmar");
-		int seleccion = jf.showSaveDialog(null);
-		File archivo = null;
-		if (seleccion == JFileChooser.APPROVE_OPTION) {
-			archivo = jf.getSelectedFile();
-		}
+    }
 
-		JOptionPane.showMessageDialog(null, "SELECCIONE LA LLAVE PRIVADA PARA FIRMAR");
-		jf.setCurrentDirectory(new File("claves/"));
-		jf.setDialogTitle("Seleccionar llave privada");
-		seleccion = jf.showSaveDialog(null);
-		boolean ok = true;
-		PrivateKey privateKey = null;
-		if (seleccion == JFileChooser.APPROVE_OPTION) {
-			Path path = Paths.get(jf.getSelectedFile().toURI());
-			byte[] bytes = Files.readAllBytes(path);
-			String pass = JOptionPane.showInputDialog(null, "Digite la password asociada a la llave privada");
+    public static void firmador() throws Exception, BadPaddingException {
+        JOptionPane.showMessageDialog(null, "SELECCIONE EL ARCHIVO A FIRMAR");
+        JFileChooser jf = new JFileChooser();
+        jf.setCurrentDirectory(new File("archivos/"));
+        jf.setDialogTitle("Seleccionar archivo a firmar");
+        int seleccion = jf.showSaveDialog(null);
+        File archivo = null;
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+            archivo = jf.getSelectedFile();
+        }
 
-			if (pass.length() < 16) {
-				for (int i = pass.length(); i < 16; i++) {
-					pass += '0';
-				}
-			} else if (pass.length() > 16) {
-				pass = pass.substring(0, 15);
-			}
+        JOptionPane.showMessageDialog(null, "SELECCIONE LA LLAVE PRIVADA PARA FIRMAR");
+        jf.setCurrentDirectory(new File("claves/"));
+        jf.setDialogTitle("Seleccionar llave privada");
+        seleccion = jf.showSaveDialog(null);
+        boolean ok = true;
+        PrivateKey privateKey = null;
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+            Path path = Paths.get(jf.getSelectedFile().toURI());
+            byte[] bytes = Files.readAllBytes(path);
+            String pass = JOptionPane.showInputDialog(null, "Digite la password asociada a la llave privada");
 
-			Encriptador enc = new Encriptador(pass, bytes);
-			try {
-				bytes = enc.desencriptar();
-			} catch (BadPaddingException e) {
-				ok = false;
-				JOptionPane.showMessageDialog(null, "Clave incorrecta", "Error", JOptionPane.ERROR_MESSAGE);
-				
-			}
+            if (pass.length() < 16) {
+                for (int i = pass.length(); i < 16; i++) {
+                    pass += '0';
+                }
+            } else if (pass.length() > 16) {
+                pass = pass.substring(0, 15);
+            }
 
-			if (ok) {
-				PKCS8EncodedKeySpec ks = new PKCS8EncodedKeySpec(bytes);
-				KeyFactory kf = KeyFactory.getInstance("RSA");
-				privateKey = kf.generatePrivate(ks);
-			}
-		}
+            Encriptador enc = new Encriptador(pass, bytes);
+            try {
+                bytes = enc.desencriptar();
+            } catch (BadPaddingException e) {
+                ok = false;
+                JOptionPane.showMessageDialog(null, "Clave incorrecta", "Error", JOptionPane.ERROR_MESSAGE);
 
-		if (ok) {
-			Firmador firmador = new Firmador(archivo, privateKey);
+            }
 
-			JOptionPane.showMessageDialog(null, "GUARDE SU FIRMA");
-			jf.setCurrentDirectory(new File("archivos/"));
-			jf.setDialogTitle("Guardar firma");
-			seleccion = jf.showSaveDialog(null);
-			if (seleccion == JFileChooser.APPROVE_OPTION) {
-				String path = jf.getSelectedFile().getAbsolutePath();
-				PrintWriter out = new PrintWriter(new File(path + ".txt"));
-				out.write(firmador.firmar());
-				out.close();
-			}
-		} else {
+            if (ok) {
+                PKCS8EncodedKeySpec ks = new PKCS8EncodedKeySpec(bytes);
+                KeyFactory kf = KeyFactory.getInstance("RSA");
+                privateKey = kf.generatePrivate(ks);
+            }
+        }
 
-		}
+        if (ok) {
+            Firmador firmador = new Firmador(archivo, privateKey);
 
-	}
+            JOptionPane.showMessageDialog(null, "GUARDE SU FIRMA");
+            jf.setCurrentDirectory(new File("archivos/"));
+            jf.setDialogTitle("Guardar firma");
+            seleccion = jf.showSaveDialog(null);
+            if (seleccion == JFileChooser.APPROVE_OPTION) {
+                String path = jf.getSelectedFile().getAbsolutePath();
+                PrintWriter out = new PrintWriter(new File(path + ".txt"));
+                out.write(firmador.firmar());
+                out.close();
+            }
+        } else {
 
-	public static void verificador() throws Exception {
-		JOptionPane.showMessageDialog(null, "SELECCIONE EL ARCHIVO A VERIFICAR");
-		JFileChooser jf = new JFileChooser();
-		jf.setCurrentDirectory(new File("archivos/"));
-		jf.setDialogTitle("Seleccionar archivo a verificar");
-		int seleccion = jf.showSaveDialog(null);
-		File archivo = null;
-		if (seleccion == JFileChooser.APPROVE_OPTION) {
-			archivo = jf.getSelectedFile();
-		}
+        }
 
-		JOptionPane.showMessageDialog(null, "SELECCIONE LA LLAVE PUBLICA");
-		jf.setCurrentDirectory(new File("claves/"));
-		jf.setDialogTitle("Seleccionar llave pÃºblica");
-		seleccion = jf.showSaveDialog(null);
+    }
 
-		PublicKey publicKey = null;
-		if (seleccion == JFileChooser.APPROVE_OPTION) {
-			Path path = Paths.get(jf.getSelectedFile().toURI());
-			byte[] bytes = Files.readAllBytes(path);
+    public static void verificador() throws Exception {
+        JOptionPane.showMessageDialog(null, "SELECCIONE EL ARCHIVO A VERIFICAR");
+        JFileChooser jf = new JFileChooser();
+        jf.setCurrentDirectory(new File("archivos/"));
+        jf.setDialogTitle("Seleccionar archivo a verificar");
+        int seleccion = jf.showSaveDialog(null);
+        File archivo = null;
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+            archivo = jf.getSelectedFile();
+        }
 
-			X509EncodedKeySpec ks = new X509EncodedKeySpec(bytes);
-			KeyFactory kf = KeyFactory.getInstance("RSA");
-			publicKey = kf.generatePublic(ks);
-		}
+        JOptionPane.showMessageDialog(null, "SELECCIONE LA LLAVE PUBLICA");
+        jf.setCurrentDirectory(new File("claves/"));
+        jf.setDialogTitle("Seleccionar llave pÃºblica");
+        seleccion = jf.showSaveDialog(null);
 
-		JOptionPane.showMessageDialog(null, "SELECCIONE LA FIRMA DEL ARCHIVO");
-		jf.setCurrentDirectory(new File("archivos/"));
-		jf.setDialogTitle("Seleccionar firma");
-		seleccion = jf.showSaveDialog(null);
+        PublicKey publicKey = null;
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+            Path path = Paths.get(jf.getSelectedFile().toURI());
+            byte[] bytes = Files.readAllBytes(path);
 
-		String firma = "";
-		if (seleccion == JFileChooser.APPROVE_OPTION) {
-			File firmita = jf.getSelectedFile();
-			BufferedReader in = new BufferedReader(new FileReader(firmita));
-			firma = in.readLine();
-			in.close();
-		}
+            X509EncodedKeySpec ks = new X509EncodedKeySpec(bytes);
+            KeyFactory kf = KeyFactory.getInstance("RSA");
+            publicKey = kf.generatePublic(ks);
+        }
 
-		Verificador verificador = new Verificador(archivo, firma, publicKey);
-		if (verificador.verificar()) {
-			JOptionPane.showMessageDialog(null, "LA FIRMA ES CORRECTA");
-		} else {
-			JOptionPane.showMessageDialog(null, "LA FIRMA ES INCORRECTA", "ERROR", JOptionPane.ERROR_MESSAGE);
-		}
+        JOptionPane.showMessageDialog(null, "SELECCIONE LA FIRMA DEL ARCHIVO");
+        jf.setCurrentDirectory(new File("archivos/"));
+        jf.setDialogTitle("Seleccionar firma");
+        seleccion = jf.showSaveDialog(null);
 
-	}
+        String firma = "";
+        if (seleccion == JFileChooser.APPROVE_OPTION) {
+            File firmita = jf.getSelectedFile();
+            BufferedReader in = new BufferedReader(new FileReader(firmita));
+            firma = in.readLine();
+            in.close();
+        }
+
+        Verificador verificador = new Verificador(archivo, firma, publicKey);
+        if (verificador.verificar()) {
+            JOptionPane.showMessageDialog(null, "LA FIRMA ES CORRECTA");
+        } else {
+            JOptionPane.showMessageDialog(null, "LA FIRMA ES INCORRECTA", "ERROR", JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
 }
